f0c5c714008e1e87668d2fb226d7cefc32649632
InterestingBrainPoops <satasiyahet@gmail.com>
2021-05-26 17:47:22 -0700

    fixed some other things, and ran cargo fmt a whole bunch of times

--- src/engine.rs
+++ src/engine.rs
@@ -1,148 +1,179 @@
-use std::{i32::MIN, str::FromStr};
+use std::i32::MIN;
 
 use crate::types::*;
 // gets the best move given a board state.
-pub fn get_move( sent_move: &Move) -> (&str, Coordinate, i32) {
-	let mut possible_moves = vec![("up", Coordinate::new(0, 1), 0 as i32), ("down", Coordinate::new(0, -1), 0 as i32), ("left", Coordinate::new(-1, 0), 0 as i32), ("right", Coordinate::new(1, 0), 0 as i32)];
-	// ^ all the possible moves ( 4 of them), their scores, and what they do.
-	for x in &mut possible_moves { // iterate through all possible moves
-		let mut board_new = sent_move.clone(); // clone the board.
-		make_move(&mut board_new, &(sent_move.you.head + x.1)); // make the move on the board
-		x.2 = eval(&board_new); // evaluate the board
-	}
-	// get the highest score, return the tuple.
-	let mut biggest = possible_moves[0];
-	for x in possible_moves {
-		println!("Move: {}, Score: {}", x.0, x.2);
-		if x.2 > biggest.2 {
-			biggest = x;
-		}
-	}
-	biggest
+pub fn get_move(sent_move: &Move) -> (&str, Coordinate, i32) {
+    let mut possible_moves = vec![
+        ("up", Coordinate::new(0, 1), 0 as i32),
+        ("down", Coordinate::new(0, -1), 0 as i32),
+        ("left", Coordinate::new(-1, 0), 0 as i32),
+        ("right", Coordinate::new(1, 0), 0 as i32),
+    ];
+    // ^ all the possible moves ( 4 of them), their scores, and what they do.
+    for x in &mut possible_moves {
+        // iterate through all possible moves
+        let mut board_new = sent_move.clone(); // clone the board.
+        make_move(&mut board_new, &(sent_move.you.head + x.1)); // make the move on the board
+        x.2 = eval(&board_new); // evaluate the board
+    }
+    // get the highest score, return the tuple.
+    let mut biggest = possible_moves[0];
+    for x in possible_moves {
+        println!("Move: {}, Score: {}", x.0, x.2);
+        if x.2 > biggest.2 {
+            biggest = x;
+        }
+    }
+    biggest
 }
 
 // higher is better
 /// Static eval of the board state.
 /// returns (reachable food) + (reachable squares) - (distance to target)
-fn eval( board : &Move) -> i32 {
-	{
-	// check if dead
-	 // if dead, then return i32 min
-	// then look at the smallest path to the closest food, if there is no path to any food, then return max negative
-	// smallest path to the head of the nearest snake that is smaller than it.
-		// make an array of all snakes
-		// remove all snakes larger than myself, and are myself
-		// find the one that has the closest head to me (A*)
-		   // if no snake is reachable, then add 0 to the score
-		   // otherwise subtract the length of the path from score.
-	// calculate food ownership
-		// iterate through each food piece
-			// see if I am the closest snake
-		// add the number of food i am closest to * 2 to score.
-	// calculate area ownership
-		// iterate through each empty square
-			// see if I am closest to that square
-		// add the number of squares i am closest to * 1 to score.
-	}
-	let mut reachable_squares = vec![];
-	flood_fill(board, &board.you.head, &mut reachable_squares);
+fn eval(board: &Move) -> i32 {
+    {
+        // check if dead
+        // if dead, then return i32 min
+        // then look at the smallest path to the closest food, if there is no path to any food, then return max negative
+        // smallest path to the head of the nearest snake that is smaller than it.
+        // make an array of all snakes
+        // remove all snakes larger than myself, and are myself
+        // find the one that has the closest head to me (A*)
+        // if no snake is reachable, then add 0 to the score
+        // otherwise subtract the length of the path from score.
+        // calculate food ownership
+        // iterate through each food piece
+        // see if I am the closest snake
+        // add the number of food i am closest to * 2 to score.
+        // calculate area ownership
+        // iterate through each empty square
+        // see if I am closest to that square
+        // add the number of squares i am closest to * 1 to score.
+    }
+    let mut reachable_squares = vec![];
+    flood_fill(board, &board.you.head, &mut reachable_squares);
 
-	if self::lost(board) {return MIN;} // if i lost, then return the minimum 
-	let mut food_scores: Vec<(usize, i32)> = vec![];// create the food scores array.
-	let target;
-	for (index, foodpos) in board.board.food.iter().enumerate(){
-		if reachable_squares.iter().any(|&i| i== *foodpos) { // checks if the food is reachable.
-			food_scores.push((index, manhattan(&board.you.head, foodpos))); // populate the foodscores array
-		}else {
-			food_scores.push((index, 10000)); // basically if the food isnt reachable, then make it so.
-		}
-	}
-	food_scores.sort_by(|a, b| a.1.cmp(&b.1)); // sort by distance, least to greatest
-	
-	// get the smallest snake
-	let mut smallest = &board.board.snakes[0];
-	for snake in &board.board.snakes { // loop through all snakes
-		if smallest.length > snake.length && snake.id.ne(&board.you.id) { // if smallest isn't the smallest, make it the smallest
-			smallest = snake;
-		}
-	}
-	if smallest.length < board.you.length && board.board.snakes.len() != 0 {
-		target = smallest.head;
-		println!("Choosing Snake at distance {}", manhattan(&board.you.head, &target))
-	}else{
-		target = board.board.food[food_scores[0].0];
-		println!("Choosing Food at distance {}", food_scores[0].1);
-	}
-	
-	println!("FF SCORE : {}", reachable_squares.len());
-	
-	food_scores.len() as i32 + reachable_squares.len() as i32 - manhattan(&board.you.head, &target)*4
+    if self::lost(board) {
+        return MIN;
+    } // if i lost, then return the minimum
+    let mut food_scores: Vec<(usize, i32)> = vec![]; // create the food scores array.
+    let target;
+    for (index, foodpos) in board.board.food.iter().enumerate() {
+        if reachable_squares.iter().any(|&i| i == *foodpos) {
+            // checks if the food is reachable.
+            food_scores.push((index, manhattan(&board.you.head, foodpos))); // populate the foodscores array
+        } else {
+            food_scores.push((index, 10000)); // basically if the food isnt reachable, then make it so.
+        }
+    }
+    food_scores.sort_by(|a, b| a.1.cmp(&b.1)); // sort by distance, least to greatest
+
+    // get the smallest snake
+    let mut smallest = &board.board.snakes[0];
+    for snake in &board.board.snakes {
+        // loop through all snakes
+        if smallest.length > snake.length && snake.id.ne(&board.you.id) {
+            // if smallest isn't the smallest, make it the smallest
+            smallest = snake;
+        }
+    }
+    if smallest.length < board.you.length && board.board.snakes.len() != 0 {
+        target = smallest.head;
+        println!(
+            "Choosing Snake at distance {}",
+            manhattan(&board.you.head, &target)
+        )
+    } else {
+        target = board.board.food[food_scores[0].0];
+        println!("Choosing Food at distance {}", food_scores[0].1);
+    }
+
+    println!("FF SCORE : {}", reachable_squares.len());
+
+    food_scores.len() as i32 + reachable_squares.len() as i32
+        - manhattan(&board.you.head, &target) * 4
 }
 // makes the following move on the board given.
 // Only applies the move to YOU.
 // Doesn't clone the board state.
-fn make_move(board : &mut Move, move_to_make : &Coordinate){
-	board.you.head = *move_to_make; // sets the head of the snake
-	board.you.body.insert(0, board.you.head); // adds the head to the beginning of the snake
-	board.you.body.pop(); // removes the tail
-	// find the right snake
-	for x in &mut board.board.snakes {
-		if x.id.eq(&board.you.id) {
-			x.head = *move_to_make;
-			x.body.insert(0, x.head);
-			x.body.pop();
-		}else{
-			x.body.pop();
-		}
-	}
+// should probably be an implementation.
+// also removes all the tails for ALL snakes, not just you.
+fn make_move(board: &mut Move, move_to_make: &Coordinate) {
+    board.you.head = *move_to_make; // sets the head of the snake
+    board.you.body.insert(0, board.you.head); // adds the head to the beginning of the snake
+    board.you.body.pop(); // removes the tail
+                          // find the right snake
+    for x in &mut board.board.snakes {
+        if x.id.eq(&board.you.id) {
+            x.head = *move_to_make;
+            x.body.insert(0, x.head);
+            x.body.pop();
+        } else {
+            x.body.pop();
+        }
+    }
 }
 /// returns the manhattan distance between the 2 points.
 fn manhattan(pos1: &Coordinate, pos2: &Coordinate) -> i32 {
-	((pos1.x - pos2.x).abs() + (pos1.y - pos2.y).abs()) as i32
+    ((pos1.x - pos2.x).abs() + (pos1.y - pos2.y).abs()) as i32
 }
 /// returns whether or not you are dead.
 fn lost(board: &Move) -> bool {
-	if board.you.head.x < 0 || board.you.head.x >= board.board.width || board.you.head.y < 0 || board.you.head.y >= board.board.height {
-		return true; // out of bounds
-	}
-	for x in &board.board.snakes {
-		if manhattan(&board.you.head, &x.head) == 1 && x.length >= board.you.length && board.you.id != x.id {
-			return true;
-		}
-		for pos in &x.body[1..] {
-			if board.you.head == *pos{
-				// collision with a snakes body part
-				return true;
-			}
-			
-		}
-	}
+    if board.you.head.x < 0
+        || board.you.head.x >= board.board.width
+        || board.you.head.y < 0
+        || board.you.head.y >= board.board.height
+    {
+        return true; // out of bounds
+    }
+    for x in &board.board.snakes {
+        if manhattan(&board.you.head, &x.head) == 1
+            && x.length >= board.you.length
+            && board.you.id != x.id
+        {
+            return true;
+        }
+        for pos in &x.body[1..] {
+            if board.you.head == *pos {
+                // collision with a snakes body part
+                return true;
+            }
+        }
+    }
 
-	false
+    false
 }
 
-// the output of this will NEVER be negative. 
-/// 4 side recursive flood fill implementation 
-/// depth limited to prevent stack overflows
-fn flood_fill (board : &Move , seed: &Coordinate, counted : &mut Vec<Coordinate>) {
-	if counted.iter().any(|&i| i== *seed) {
-		return;
-	}
-	if seed.x < 0 || seed.x >= board.board.width || seed.y < 0 || seed.y >= board.board.height {
-		// println!("happened, out of bounds");
-		return ; // out of bounds
-	}
-	for x in &board.board.snakes {
-		for pos in &x.body[..] {
-			if *seed == *pos && *seed != board.you.head{
-				// not in bounds
-				return;
-			}
-		}
-	}
-	counted.push(*seed);
-	flood_fill(board, &(*seed + Coordinate::new(0,1)),  counted);
-	flood_fill(board, &(*seed + Coordinate::new(0,-1)), counted);
-	flood_fill(board, &(*seed + Coordinate::new(-1,0)), counted);
-	flood_fill(board, &(*seed + Coordinate::new(1,0)),counted);
+// the output of this will NEVER be negative.
+/// 4 side recursive flood fill implementation
+/// Also fills in the counted array, which is what its "output" is.
+fn flood_fill(board: &Move, seed: &Coordinate, counted: &mut Vec<Coordinate>) {
+    if counted.iter().any(|&i| i == *seed) {
+        return;
+    }
+    if seed.x < 0 || seed.x >= board.board.width || seed.y < 0 || seed.y >= board.board.height {
+        // println!("happened, out of bounds");
+        return; // out of bounds
+    }
+    for x in &board.board.snakes {
+        for pos in &x.body[..] {
+            if *seed == *pos && *seed != board.you.head {
+                // not in bounds
+                return;
+            }
+        }
+    }
+    counted.push(*seed);
+    flood_fill(board, &(*seed + Coordinate::new(0, 1)), counted);
+    flood_fill(board, &(*seed + Coordinate::new(0, -1)), counted);
+    flood_fill(board, &(*seed + Coordinate::new(-1, 0)), counted);
+    flood_fill(board, &(*seed + Coordinate::new(1, 0)), counted);
+}
+//TODO:
+// make the move gen per snake. (presumably an implented function)
+// move gen per turn (ie if its my turn or the "other"'s turn.)
+// minimax implemented.
+// alpha beta implemented.
+fn minimax(state: &mut Move, depth: i32, maximizing: bool) -> i32 {
+    0
 }
--- src/types.rs
+++ src/types.rs
@@ -14,12 +14,6 @@ pub struct SentGame {
     pub timeout: u128,
 }
 
-
-#[derive(Debug,Deserialize, Clone)]
-pub struct Ruleset {
-    pub name : String,
-    pub version: String,
-}
 #[derive(Debug, Deserialize, Clone)]
 pub struct Board {
     pub height: i8,
@@ -40,11 +34,98 @@ pub struct Battlesnake {
     pub length: u16,
     pub shout: String,
 }
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct Delta {
+    pub dead: Vec<Battlesnake>, // the snakes that died during this turn
+    pub tails: Vec<(String, Coordinate)>, // the tails of the snakes that were removed during this turn
+    pub eaten_food: Vec<Coordinate>, // the positions of the food that were eaten during this turn ( if any )
+}
+impl Move {
+    pub fn make_move(&mut self, moves: &Vec<SnakeMove>, turn: bool) -> Delta {
+        let mut out = Delta {
+            dead: vec![],
+            tails: vec![],
+            eaten_food: vec![],
+        };
+        let mut all_alive: Vec<Battlesnake> = vec![];
+        for snake in &mut self.board.snakes {
+            // each snake
+            snake.health -= 1; // decrement the health
+            for snakes_move in moves {
+                // this entire block here just moves the snakes in the direction they chose
+                if snake.id == snakes_move.id {
+                    // basically if it matches the id.
+                    let add = match snakes_move.snake_move.as_str() {
+                        "up" => Coordinate::new(0, 1),
+                        "down" => Coordinate::new(0, -1),
+                        "left" => Coordinate::new(-1, 0),
+                        "right" => Coordinate::new(1, 0),
+                        _ => {
+                            panic!("A move was not UDLR");
+                        }
+                    };
+                    snake.head += add;
+                    snake.body.insert(0, snake.head);
+                    match snake.body.pop() {
+                        Some(x) => {
+                            out.tails.push((snake.id.clone(), x));
+                        }
+                        None => panic!("snakes were at length zero. This shouldn't happen."),
+                    }
+                }
+            }
+            // checks if the head is on any food, and if it is, then it removes it.
+            match self.board.food.iter().position(|&r| r == snake.head) {
+                Some(index) => {
+                    out.eaten_food.push(self.board.food.remove(index)); // removes the food at the given index.
+                    snake.health = 100;
+                    snake.body.push(snake.body[snake.body.len() - 1]); // basically dupes the tail.
+                }
+                None => {}
+            }
+        }
+		for snake in &self.board.snakes {
+			let mut alive = true; // basically whether or not this snake is dead.
+            if snake.health <= 0 {
+                // no health
+                alive = false;
+            } else if snake.head.x < 0
+                || snake.head.y < 0
+                || snake.head.x >= self.board.width
+                || snake.head.y >= self.board.height
+            {
+                // out of bounds
+                alive = false;
+            }
+			if alive {
+				for x in &self.board.snakes {
+                    for y in &x.body[1..] {
+                        if *y == snake.head {
+                            alive = false;
+                        }
+                    }
+                }
+                alive = true;
+            }
+            if alive {
+                all_alive.push(snake.clone());
+            }
+		}
+        out
+    }
+}
 #[derive(Debug, Deserialize, Eq, PartialEq, Clone, Copy)]
 pub struct Coordinate {
     pub x: i8,
     pub y: i8,
 }
+
+#[derive(Debug, Deserialize, Eq, PartialEq, Clone)]
+pub struct SnakeMove {
+    pub snake_move: String,
+    pub id: String,
+}
+
 impl ops::AddAssign<Coordinate> for Coordinate {
     fn add_assign(&mut self, rhs: Coordinate) {
         self.x += rhs.x;
@@ -54,11 +135,11 @@ impl ops::AddAssign<Coordinate> for Coordinate {
 impl ops::Add<Coordinate> for Coordinate {
     type Output = Coordinate;
     fn add(self, rhs: Coordinate) -> Self::Output {
-        Coordinate::new(self.x + rhs.x , self.y + rhs.y)
+        Coordinate::new(self.x + rhs.x, self.y + rhs.y)
     }
 }
 impl Coordinate {
-    pub fn new(x : i8, y: i8) -> Self {
-        Coordinate{x, y}
+    pub fn new(x: i8, y: i8) -> Self {
+        Coordinate { x, y }
     }
 }
