bf36a3d08480e210afaceab7b008987a361571c2
InterestingBrainPoops <satasiyahet@gmail.com>
2021-06-22 15:14:30 -0700

    cleaned up comments, finish unmake move

--- src/types.rs
+++ src/types.rs
@@ -113,7 +113,7 @@ impl State {
         //   head to head collision
         for snake in &self.state.board.snakes {
             if !self.dead.contains(&snake.id) {
-                if snake.health <= 0 {
+                if snake.health <= 0 { // out of health
                     self.dead.push(snake.id.clone());
                     out.died.push(snake.id.clone());
                     // no health
@@ -121,13 +121,13 @@ impl State {
                     || snake.head.y < 0
                     || snake.head.x >= self.state.board.width
                     || snake.head.y >= self.state.board.height
-                {
+                { // out of bounds
                     self.dead.push(snake.id.clone());
                     out.died.push(snake.id.clone());
                     // out of bounds
                 } else {
                     for opp in &self.state.board.snakes {
-                        // the following is to check if my head is within their body.
+                        // the following is to check if snake  head is within opp's body.
                         if !self.dead.contains(&opp.id) && opp.body.contains(&snake.head) {
                             self.dead.push(snake.id.clone());
                             out.died.push(snake.id.clone());
@@ -141,23 +141,24 @@ impl State {
     }
     pub fn unmake_move(&mut self, delta: &Delta) {
         // add tails back to snakes
+        for tail in &delta.tails {
+            for snake in &mut self.state.board.snakes {
+                if tail.0 == snake.id {
+                    snake.body.insert(0,tail.1);
+                }
+            }
+        }
         // remove the heads
         // increase all snake health by 1
+        for snake in &mut self.state.board.snakes {
+            snake.health += 1;
+            snake.body.pop();
+        }
         // revive all killed snakes
+        self.dead.retain(|x| !delta.died.contains(x));
+        // put all food back
+        self.state.board.food.append(&mut delta.eaten_food.clone());
     }
-    // function minimax(node, depth, maximizingPlayer) is
-    // if depth = 0 or node is a terminal node then
-    //     return the heuristic value of node
-    // if maximizingPlayer then
-    //     value := −∞
-    //     for each child of node do
-    //         value := max(value, minimax(child, depth − 1, FALSE))
-    //     return value
-    // else (* minimizing player *)
-    //     value := +∞
-    //     for each child of node do
-    //         value := min(value, minimax(child, depth − 1, TRUE))
-    //     return value
 
     /// Depth is how far to search
     /// maximizing is whether the function is supposed to be maximizing or minimizing.
