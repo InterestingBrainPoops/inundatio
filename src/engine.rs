92ab48084316d72674b023944dd7bb3ab4113980
InterestingBrainPoops <satasiyahet@gmail.com>
2021-07-18 14:50:40 -0700

    add floodfill

--- src/engine.rs
+++ src/engine.rs
@@ -18,7 +18,7 @@ pub fn eval(board: &SmallMove) -> i32 {
     }
     (board.you.length * 1500) as i32
         - ((board.board.snakes.len() - amnt_dead(board)) * 5) as i32
-        - closest_pos.1 * 30
+        - closest_pos.1 * 30 + (flood_fill(board)*30) as i32
 }
 
 fn amnt_dead(board: &SmallMove) -> usize {
@@ -35,30 +35,95 @@ fn manhattan(pos1: &Coordinate, pos2: &Coordinate) -> i32 {
     ((pos1.x - pos2.x).abs() + (pos1.y - pos2.y).abs()) as i32
 }
 
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Filled {
+    Empty,
+    Full
+}
 // the output of this will NEVER be negative.
 /// 4 side recursive flood fill implementation
 /// Also fills in the counted array, which is what its "output" is.
-fn flood_fill(board: &Move, seed: &Coordinate, counted: &mut Vec<Coordinate>, dead: &Vec<String>) {
-    if counted.iter().any(|&i| i == *seed) {
-        return; // already reached
-    }
-    if seed.x < 0 || seed.x >= board.board.width || seed.y < 0 || seed.y >= board.board.height {
-        return; // out of bounds
-    }
+fn flood_fill(board: &SmallMove) -> u32 {
+    let mut all = vec![vec![Filled::Empty;board.board.width as usize];board.board.height as usize];
     for x in &board.board.snakes {
-        if dead.contains(&x.id) {
-            continue; // doesnt check against dead snakes.
-        }
-        for pos in &x.body[..] {
-            if *seed == *pos && *seed != board.you.head {
-                // not in bounds
-                return;
-            }
+        for y  in &x.body {
+            all[y.x as usize][y.y as usize] = Filled::Empty;
         }
     }
-    counted.push(*seed);
-    flood_fill(board, &(*seed + Coordinate::new(0, 1)), counted, dead);
-    flood_fill(board, &(*seed + Coordinate::new(0, -1)), counted, dead);
-    flood_fill(board, &(*seed + Coordinate::new(-1, 0)), counted, dead);
-    flood_fill(board, &(*seed + Coordinate::new(1, 0)), counted, dead);
+    let mut x1;
+    let mut span_above;
+    let mut span_below;
+    let mut out = 0;
+    let mut stack:Vec<Coordinate> = vec![];
+    stack.push(board.you.head);
+    while stack.len() > 0 {
+        let thing = stack.pop().expect("");
+        x1 = thing.x;
+        while x1 >= 0 && all[thing.x as usize][thing.y as usize] != Filled::Full {x1 -= 1;}
+        x1 += 1;
+        span_above = false; 
+        span_below = false;
+        while x1 < thing.x && all[thing.x as usize][thing.y as usize] != Filled::Full {
+            all[thing.x as usize][thing.y as usize] = Filled::Full;
+            out += 1;
+            if !span_above && thing.y > 0 && all[thing.x as usize][thing.y as usize] == Filled::Empty {
+                stack.push(Coordinate::new(x1 , thing.y - 1));
+                span_above = true;
+            }else if span_above && thing.y > 0 && all[thing.x as usize][thing.y as usize] == Filled::Full {
+                span_above = false;
+            }
+
+            if !span_below && thing.y < board.board.height - 1 && all[thing.x as usize][thing.y as usize] == Filled::Empty {
+                stack.push(Coordinate::new(x1, thing.y + 1));
+                span_below = true;
+            } 
+            else if span_below && thing.y < board.board.height - 1 && all[thing.x as usize][thing.y as usize] != Filled::Full {
+                span_below = false;
+            }
+            x1 += 1;
+        }
+    }
+    out
+
 }
+// void floodFillScanlineStack(int x, int y, int newColor, int oldColor)
+// {
+//   if(oldColor == newColor) return;
+
+//   int x1;
+//   bool spanAbove, spanBelow;
+
+//   std::vector<int> stack;
+//   push(stack, x, y);
+//   while(pop(stack, x, y))
+//   {
+//     x1 = x;
+//     while(x1 >= 0 && screenBuffer[y * w + x1] == oldColor) x1--;
+//     x1++;
+//     spanAbove = spanBelow = 0;
+//     while(x1 < w && screenBuffer[y * w + x1] == oldColor)
+//     {
+//       screenBuffer[y * w + x1] = newColor;
+//       if(!spanAbove && y > 0 && screenBuffer[(y - 1) * w + x1] == oldColor)
+//       {
+//         push(stack, x1, y - 1);
+//         spanAbove = 1;
+//       }
+//       else if(spanAbove && y > 0 && screenBuffer[(y - 1) * w + x1] != oldColor)
+//       {
+//         spanAbove = 0;
+//       }
+//       if(!spanBelow && y < h - 1 && screenBuffer[(y + 1) * w + x1] == oldColor)
+//       {
+//         push(stack, x1, y + 1);
+//         spanBelow = 1;
+//       }
+//       else if(spanBelow && y < h - 1 && screenBuffer[(y + 1) * w + x1] != oldColor)
+//       {
+//         spanBelow = 0;
+//       }
+//       x1++;
+//     }
+//   }
+// }
